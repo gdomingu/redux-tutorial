f24028aab9d3b53929c518bd8646c7ce1ff1a0e4
Gabe D <gpad88@gmail.com>
2019-04-16 12:46:37 -0700

    Adds archive action to redux store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import store from './store';
+import { STORY_ARCHIVE } from './constants/actionTypes';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App stories={store.getState().storyState} onArchive={() => {}} />, document.getElementById('root'));
+ReactDOM.render(
+  <App
+    stories={store.getState().storyState}
+    onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id})}
+  />,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
